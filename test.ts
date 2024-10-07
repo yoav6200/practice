fa83641b29ffd9d5de728fffbc7787d5b0a1ee0f
Yoav Herman <yoavh@moveo.co.il>
2024-10-07 15:25:47 +0300

    first commit

---
+++ test.ts
@@ -0,0 +1,14 @@
+const BASE=50;
+
+class Product{
+    name: string;
+    cost: number;
+    isAvailable: boolean
+
+    constructor(name:string,cost:number){
+        this.name=name;
+        this.cost=cost;
+        this.isAvailable=true;
+    }
+}
+
